418a69a9b91a33cb0deac10f386e6bfa8b38a939
ml1238 <98048589+ml1238@users.noreply.github.com>
2022-01-21 11:49:29 -0700

    Updated Programs Folder
    An Updated Programs folder with several new files, and a simple java program.

---
+++ Programs/HelloWorld/helloworld.java
@@ -0,0 +1,14 @@
+// Mike Lopez III
+// CS371
+// Lab1
+// 01/21/2022
+
+public class helloworld {
+
+   public static void main( String args[] ) {
+   
+      System.out.println("Hello World");
+   
+   } // end main
+   
+} // end class
